951bae781a64f665d9b693a3084f57fce2a013ed
lenovo <novakova.elitca@gmail.com>
2021-03-19 18:06:15 +0200

    GossipControllerTest.java

--- src/test/java/bg/codeacademy/spring/gossiptalks/LoginTest.java
+++ src/test/java/bg/codeacademy/spring/gossiptalks/LoginTest.java
@@ -1,6 +1,6 @@
 package bg.codeacademy.spring.gossiptalks;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
