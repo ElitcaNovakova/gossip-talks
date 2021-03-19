2ec7a7fa0a938c4a39f71df0664b74a9f498a7e1
lenovo <novakova.elitca@gmail.com>
2021-03-19 17:47:10 +0200

    Changes UserRepository.java

--- src/main/java/bg/codeacademy/spring/gossiptalks/repository/UserRepository.java
+++ src/main/java/bg/codeacademy/spring/gossiptalks/repository/UserRepository.java
@@ -2,13 +2,11 @@ package bg.codeacademy.spring.gossiptalks.repository;
 
 import bg.codeacademy.spring.gossiptalks.model.User;
 import java.util.List;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
-
 
   User findByUsername(String username);
 
@@ -16,7 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
   List<User> findByNameContainsIgnoreCase(String name);
 
-  List<User> findByFollowers_Id(long id);
-
+  List<User> findByFollowers_Id(Long id);
 
 }
