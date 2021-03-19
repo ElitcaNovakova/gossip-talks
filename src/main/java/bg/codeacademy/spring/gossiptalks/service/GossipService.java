ad9fd950686c4e221d0105d30d4eb83cc8499a17
lenovo <novakova.elitca@gmail.com>
2021-03-19 17:48:03 +0200

    Changes GossipService.java

--- src/main/java/bg/codeacademy/spring/gossiptalks/service/GossipService.java
+++ src/main/java/bg/codeacademy/spring/gossiptalks/service/GossipService.java
@@ -6,6 +6,7 @@ import bg.codeacademy.spring.gossiptalks.repository.GossipRepository;
 import bg.codeacademy.spring.gossiptalks.repository.UserRepository;
 import java.time.OffsetDateTime;
 import java.util.List;
+import javax.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -21,8 +22,7 @@ public class GossipService {
 
 
   public GossipService(UserRepository userRepository,
-      GossipRepository gossipRepository)
-  {
+      GossipRepository gossipRepository) {
     this.userRepository = userRepository;
     this.gossipRepository = gossipRepository;
 
@@ -30,36 +30,28 @@ public class GossipService {
 
   // find gossips of particular user
   public Page<Gossip> getGossipsByAuthor(int pageNo, int pageSize, String author) {
-    Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by("dateTime"));
+    Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by("Id"));
     return gossipRepository.findByAuthor_Username(author, pageable);
   }
 
 
   // find gossips of the friends
-  public Page<Gossip> getGossips(Integer pageNo, Integer pageSize, User user) {
-    Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by("dateTime"));
+  public Page<Gossip> getGossips(Integer pageNo, Integer pageSize, @NotNull User user) {
+    Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by("Id"));
     // 1. get Users, that you follow
-      List<User> friends = userRepository.findByFollowers_Id(user.getId());
-    
+    List<User> friends = userRepository.findByFollowers_Id(user.getId());
     // 2. get Gossips of those Users
-    return gossipRepository.findByAuthorIn(friends, pageable);
+    return gossipRepository.findByAuthorInOrderByDateTime(friends, pageable);
   }
 
   public Gossip createGossip(String text, User user) {
     OffsetDateTime dateTime = OffsetDateTime.now();
-
-    Gossip gossip = new Gossip();
-    gossip.setAuthor(user);
-    gossip.setText(text);
-    gossip.setDateTime(dateTime);
     gossipRepository.save(new Gossip()
-                                .setAuthor(user)
-                                .setDateTime(dateTime)
-                                .setText(text));
-       List<Gossip> gossipList = gossipRepository.findByAuthor_Id(user.getId());
+        .setAuthor(user)
+        .setDateTime(dateTime)
+        .setText(text));
+    List<Gossip> gossipList = gossipRepository.findByAuthor_Id(user.getId());
     int index = gossipList.size();
-    return gossipList.get(index-1);
-
+    return gossipList.get(index - 1);
   }
-
 }
